32bdea1bad6ca57668b1fd7556c4bc591bf68e94
王咸海 <hx0011047@hengxinyongli.com>
2019-08-19 17:57:40 +0800

    update:修改配置，优化服务发现过慢

--- order-service/src/main/java/com/as/order/controller/OrderController.java
+++ order-service/src/main/java/com/as/order/controller/OrderController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/order")
+@RestController
+@RequestMapping("/order")
 @Slf4j
 public class OrderController {
 
